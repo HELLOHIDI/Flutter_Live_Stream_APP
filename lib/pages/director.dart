3bb35bb6381f4f473098d9e187176a7095badc67
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2022-03-27 23:07:46 +0900

    Setting up State

--- lib/pages/director.dart
+++ lib/pages/director.dart
@@ -1,20 +1,222 @@
+import 'package:agora_rtc_engine/rtc_engine.dart';
+// ignore: library_prefixes
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+// ignore: library_prefixes
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+import 'package:agora_rtm/agora_rtm.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Director extends StatefulWidget {
-  final String channelName;
+import '../appId.dart';
+import 'package:flutterlivestreamapp/models/user.dart';
 
-  const Director({Key? key, required this.channelName}) : super(key: key);
-  _DirectorState createState() => _DirectorState();
+class Participant extends StatefulWidget {
+  final String channelName;
+  final String userName;
+  final int uid;
+  const Participant({
+    Key? key,
+    required this.channelName,
+    required this.userName,
+    required this.uid,
+  }) : super(key: key);
+  _ParticipantState createState() => _ParticipantState();
 }
 
-class _DirectorState extends State<Director> {
+class _ParticipantState extends State<Participant> {
+  final List<AgoraUser> _users = [];
+  late RtcEngine _engine; //실제화상통화?
+  AgoraRtmClient? _client;
+  AgoraRtmChannel? _channel;
+  bool muted = false;
+  bool videoDisabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeAgora();
+  }
+
+  @override
+  void dispose() {
+    _users.clear();
+    _engine.leaveChannel();
+    _engine.destroy();
+    _channel?.leave();
+    _client?.logout();
+    _client?.destroy();
+
+    super.dispose();
+  }
+
+  Future<void> initializeAgora() async {
+    _engine = await RtcEngine.createWithContext(RtcEngineContext(appId));
+    _client = await AgoraRtmClient.createInstance(appId);
+
+    await _engine.enableVideo();
+    await _engine.setChannelProfile(ChannelProfile.LiveBroadcasting);
+    await _engine.setClientRole(ClientRole.Broadcaster);
+
+    // Callbacks for the RTC ENGINE
+    _engine.setEventHandler(
+      RtcEngineEventHandler(
+          //TODO: Add join channel logic
+          joinChannelSuccess: (channel, uid, elapsed) {
+        setState(() {
+          _users.add(AgoraUser(uid: uid));
+        });
+      }, leaveChannel: (stats) {
+        setState(() {
+          _users.clear();
+        });
+      }),
+    );
+
+    // Callbacks for RTM Client
+    _client?.onMessageReceived = (AgoraRtmMessage message, String peerId) {
+      print("Private Message from " + peerId + ":" + (message.text));
+    };
+
+    _client?.onConnectionStateChanged = (int state, int reason) {
+      print('Connection state changed:  ' + state.toString() + ', reason:  ' + reason.toString());
+      if (state == 5) {
+        _channel?.leave();
+        _client?.logout();
+        _client?.destroy();
+        print("Logout.");
+      }
+    };
+
+    // Callbacs for RTM Channel
+    _channel?.onMemberJoined = (AgoraRtmMember member) {
+      print("Member joined: " + member.userId + ', channel: ' + member.channelId);
+    };
+
+    _channel?.onMemberLeft = (AgoraRtmMember member) {
+      print("Member left: " + member.userId + ', channel: ' + member.channelId);
+    };
+
+    _channel?.onMessageReceived = (AgoraRtmMessage message, AgoraRtmMember member) {
+      //TODO: implement this
+      print("Public Message from " + member.userId + ": " + (message.text));
+    };
+
+    //Join the RTM and RTC Channels
+    await _client?.login(null, widget.uid.toString());
+    _channel = await _client?.createChannel(widget.channelName);
+    await _channel?.join();
+    await _engine?.joinChannel(null, widget.channelName, null, widget.uid);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text("Director"),
+        child: Stack(
+          children: [
+            _broadcastView(),
+            _toolbar()
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _toolbar() {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: const EdgeInsets.symmetric(vertical: 48),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RawMaterialButton(
+            onPressed: _onToggleMute,
+            child: Icon(
+              muted ? Icons.mic_off : Icons.mic,
+              color: muted ? Colors.white : Colors.blueAccent,
+              size: 20.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: muted ? Colors.blueAccent : Colors.white,
+            padding: const EdgeInsets.all(12.0),
+          ),
+          RawMaterialButton(
+            onPressed: () => _onCallEnd(context),
+            child: Icon(
+              Icons.call_end,
+              color: Colors.white,
+              size: 35.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: Colors.redAccent,
+            padding: const EdgeInsets.all(15.0),
+          ),
+          RawMaterialButton(
+            onPressed: _onToggleVideoDisabled,
+            child: Icon(
+              videoDisabled ? Icons.videocam_off : Icons.videocam,
+              color: videoDisabled ? Colors.white : Colors.blueAccent,
+              size: 20.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: videoDisabled ? Colors.blueAccent : Colors.white,
+            padding: const EdgeInsets.all(12.0),
+          ),
+          RawMaterialButton(
+            onPressed: _onSwitchCamera,
+            child: Icon(
+              Icons.switch_camera,
+              color: Colors.blueAccent,
+              size: 20.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: Colors.white,
+            padding: const EdgeInsets.all(12.0),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _broadcastView() {
+    if (_users.isEmpty) {
+      return const Center(
+        child: Text("No Users"),
+      );
+    }
+    return Row(
+      children: [
+        Expanded(
+          child: RtcLocalView.SurfaceView(),
+        ),
+      ],
+    );
+  }
+
+  void _onToggleMute() {
+    setState(() {
+      muted = !muted;
+    });
+    _engine.muteLocalAudioStream(muted);
+  }
+
+  void _onToggleVideoDisabled() {
+    setState(() {
+      videoDisabled = !videoDisabled;
+    });
+    _engine.muteLocalVideoStream(videoDisabled);
+  }
+
+  void _onSwitchCamera() {
+    _engine.switchCamera();
+  }
+
+  void _onCallEnd(BuildContext context) {
+    Navigator.pop(context);
   }
 }
