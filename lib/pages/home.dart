20786d99901b96b16992388a418116e44bcc4dae
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2022-03-28 23:30:54 +0900

    Joining the Live Stream

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -108,6 +108,7 @@ class _HomeState extends State<Home> {
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (context) => Director(
+                      uid: uid,
                       channelName: _channelName.text,
                     ),
                   ),
