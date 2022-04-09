1d5a909f02c4394504aa3a52e306628e8a94a789
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2022-04-09 12:31:26 +0900

    Comment on 'Joining the live stream' part

--- lib/controllers/director_controller.dart
+++ lib/controllers/director_controller.dart
@@ -21,19 +21,20 @@ class DirectorController extends StateNotifier<DirectorModel> {
 
   DirectorController(this.read) : super(DirectorModel());
 
+  // 초기화 함수
   Future<void> _initialize() async {
-    RtcEngine _engine = await RtcEngine.createWithContext(RtcEngineContext(appId));
-    AgoraRtmClient _client = await AgoraRtmClient.createInstance(appId);
-    state = DirectorModel(engine: _engine, client: _client);
+    RtcEngine _engine = await RtcEngine.createWithContext(RtcEngineContext(appId)); // 엔진 생성
+    AgoraRtmClient _client = await AgoraRtmClient.createInstance(appId); //클라이언트 생성
+    state = DirectorModel(engine: _engine, client: _client); // 디렉터모델 클래스 변수 생성
   }
 
   Future<void> joinCall({required String channelName, required int uid}) async {
     await _initialize();
-    await state.engine?.enableVideo();
-    await state.engine?.setChannelProfile(ChannelProfile.LiveBroadcasting);
-    await state.engine?.setClientRole(ClientRole.Broadcaster);
+    await state.engine?.enableVideo(); // 코어 RTC는 오디오로만 시작, 그래서 비디오를 활성화시킨다.
+    await state.engine?.setChannelProfile(ChannelProfile.LiveBroadcasting); //세개의 프로필(생방송, 일반 영상통화, 게임) 중 생방송을 활성화시킨다.
+    await state.engine?.setClientRole(ClientRole.Broadcaster); // 2개의 역할(방송자, 시청자) 중 방송자를 활성화 시킨다.
 
-    // Callbacks for the RTC ENGINE
+    // Participant.dart 와 유사
     state.engine?.setEventHandler(
       RtcEngineEventHandler(
         //TODO: Add join channel logic
@@ -43,10 +44,13 @@ class DirectorController extends StateNotifier<DirectorModel> {
         leaveChannel: (stats) {
           print("Channel Left");
         },
+        // 참가자를 로비에서 화상채팅방으로
         userJoined: (uid, elapsed) {
           print("USER JOINED " + uid.toString());
           addUserToLobby(uid: uid);
         },
+
+        // 참가자를 오프라인(제거)으로
         userOffline: (uid, reason) {
           removeUser(uid: uid);
         },
@@ -84,6 +88,7 @@ class DirectorController extends StateNotifier<DirectorModel> {
 
     //Joined the RTM and RTC channels
     await state.client?.login(null, uid.toString());
+    // copywith로 기존것 유지하기
     state = state.copyWith(channel: await state.client?.createChannel(channelName));
     await state.channel?.join();
     await state.engine?.joinChannel(null, channelName, null, uid);
@@ -109,6 +114,7 @@ class DirectorController extends StateNotifier<DirectorModel> {
     await state.engine?.joinChannel(null, channelName, null, uid);
   }
 
+  // 화상 통화를 종료하는 함수 (participant의 dispose와 기능 비슷)
   Future<void> leaveCall() async {
     state.engine?.leaveChannel();
     state.engine?.destroy();
@@ -117,8 +123,10 @@ class DirectorController extends StateNotifier<DirectorModel> {
     state.client?.destroy();
   }
 
+  // 로비에 사용자를 추가하는 함수
   Future<void> addUserToLobby({required int uid}) async {
     var userAttributes = await state.client?.getUserAttributes(uid.toString());
+    // 기존 로비 유저에 아고라 유저를 추가한다.
     state = state.copyWith(
       lobbyUsers: {
         ...state.lobbyUsers,
@@ -134,19 +142,25 @@ class DirectorController extends StateNotifier<DirectorModel> {
     state.channel!.sendMessage(AgoraRtmMessage.fromText(Message().sendActiveUsers(activeUsers: state.activeUsers)));
   }
 
+  // 화상 통화 활성 사용자로 승격하는 함수
   Future<void> promoteToActiveUser({required int uid}) async {
     Set<AgoraUser> _tempLobby = state.lobbyUsers;
     Color? tempColor;
     String? tempName;
 
+    // 로비 사용자 리스트 반복문을 돌면서
     for (int i = 0; i < _tempLobby.length; i++) {
+      // 같은 uid(활성 사용자로 승격하려는 대상)이 있으면
       if (_tempLobby.elementAt(i).uid == uid) {
         tempColor = _tempLobby.elementAt(i).backgroundColor;
         tempName = _tempLobby.elementAt(i).name;
-        _tempLobby.remove(_tempLobby.elementAt(i));
+        _tempLobby.remove(_tempLobby.elementAt(i)); // 로비 사용자에서 지운다.
       }
     }
 
+    // 활성 사용자 리스트에 agora user(활성 사용자로 승격하려는 대상을)
+    // 기존 활성 사용자에 추가한다.
+    // 그리고 그 활성 사용자가 빠진 로비 사용자 리스트도 업데이트 해준다.
     state = state.copyWith(activeUsers: {
       ...state.activeUsers,
       AgoraUser(
@@ -165,19 +179,25 @@ class DirectorController extends StateNotifier<DirectorModel> {
     }
   }
 
+  // 사용자를 로비로 강등하는 함수.
   Future<void> demoteToLobbyUser({required int uid}) async {
     Set<AgoraUser> _tempActive = state.lobbyUsers;
     Color? tempColor;
     String? tempName;
 
+    // 화상통화 활성화 사용자 리스트 반복문을 돌면서
     for (int i = 0; i < _tempActive.length; i++) {
       if (_tempActive.elementAt(i).uid == uid) {
+        // 같은 uid(로비 사용자로 강등하려는 대상)이 있으면
         tempColor = _tempActive.elementAt(i).backgroundColor;
         tempName = _tempActive.elementAt(i).name;
-        _tempActive.remove(_tempActive.elementAt(i));
+        _tempActive.remove(_tempActive.elementAt(i)); // 활성 사용자에서 지운다.
       }
     }
 
+    // 로비 사용자 리스트에 agora user(로비 사용자로 강등하려는 대상을)
+    // 기존 로비 사용자에 추가한다.
+    // 그리고 그 로비 사용자가 빠진 활성 사용자 리스트도 업데이트 해준다.
     state = state.copyWith(lobbyUsers: {
       ...state.lobbyUsers,
       AgoraUser(
@@ -198,21 +218,26 @@ class DirectorController extends StateNotifier<DirectorModel> {
     }
   }
 
+  // 로비와 화상통화에서 같이 사용자를 지우는 함수
   Future<void> removeUser({required int uid}) async {
-    Set<AgoraUser> _tempActive = state.activeUsers;
-    Set<AgoraUser> _tempLobby = state.lobbyUsers;
+    Set<AgoraUser> _tempActive = state.activeUsers; // 화상 통화 활성화 유저
+    Set<AgoraUser> _tempLobby = state.lobbyUsers; // 로비 유저
 
+    // 반복문을 돌면서 같은 uid의 사용자를 제거한다.
     for (int i = 0; i < _tempActive.length; i++) {
       if (_tempActive.elementAt(i).uid == uid) {
         _tempActive.remove(_tempActive.elementAt(i));
       }
     }
 
+    // 반복문을 돌면서 같은 uid의 사용자를 제거한다.
     for (int i = 0; i < _tempLobby.length; i++) {
       if (_tempLobby.elementAt(i).uid == uid) {
         _tempLobby.remove(_tempLobby.elementAt(i));
       }
     }
+
+    // 제거한 사용자의 리스트로 업데이트 한다.
     state = state.copyWith(activeUsers: _tempActive, lobbyUsers: _tempLobby);
     state.channel!.sendMessage(AgoraRtmMessage.fromText(Message().sendActiveUsers(activeUsers: state.activeUsers)));
 
