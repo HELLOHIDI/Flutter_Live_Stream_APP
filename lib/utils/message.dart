5c63535190a68fc07f70c857e538c021b7885c79
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2022-04-09 18:42:13 +0900

    Comment on 'Control user Audio & Video' part

--- lib/utils/message.dart
+++ lib/utils/message.dart
@@ -1,8 +1,9 @@
 import 'package:flutterlivestreamapp/models/user.dart';
 
+// 모든 활성 사용자 목록을 보내는 작업을 조금 더 쉽게 하기 위해 사용될 것
 class Message {
   String sendActiveUsers({required Set<AgoraUser> activeUsers}) {
-    // ex) activeUsers uid,uid,uid,uid,
+    // ex) "activeUsers uid,uid,uid,uid,"
     String _userString = "activeUsers ";
     for (int i = 0; i < activeUsers.length; i++) {
       _userString = _userString + activeUsers.elementAt(i).uid.toString() + ",";
@@ -10,7 +11,7 @@ class Message {
     return _userString;
   }
 
-  /// uid,uid,uid,uid,
+  /// int형 [uid, uid, uid]
   List<AgoraUser> parseActiveUsers({required String uids}) {
     List<String> activeUsers = uids.split(",");
     List<AgoraUser> users = [];
